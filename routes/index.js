79f8e1a8ae8a7664d1505d06d32816d0c7dbbd91
ltoniut <ltoniut@gmail.com>
2017-06-04 19:40:57 -0300

    Missing element to former commit

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var form = require('../components/brandForm');
-var router = express.Router();
 var ReactDOM = require('react-dom');
+var React = require('react');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  ReactDOM.render(form, { title: 'Form' });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
